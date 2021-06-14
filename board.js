745ff8e331869c7d83a1f1ba94ee1a14b23290b9
Адель <adel.absalyamov@yandex.ru>
2021-06-14 15:55:47 +0300

    hover-board

---
+++ board.js
@@ -0,0 +1,31 @@
+const board = document.getElementById('board')
+const colors = ['#ffff00', '#708090', '#4b0082', '#f2f3f4', '#00ffee', '#7a61ab', '#f76c70']
+const SQUARES_NUMBER = 1665
+
+for(let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(e) {
+    const element = e.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(e) {
+    const element = e.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
